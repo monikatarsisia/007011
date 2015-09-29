18b1eb07bc007a1dc861363d8533fce6a47fcfed
Monika Tarsisia Nangoi <monikatarsisia@hotmail.com>
2015-09-29 08:06:51 -0700

    Update Parsing.py

--- Parsing.py
+++ Parsing.py
@@ -14,12 +14,15 @@ soup = BeautifulSoup(html_doc)
 
 savelink = open("hyperlink.html", 'w')
 
+data = 1
+
 for link in soup.findAll('a'):
 	anotherlink =link.get('href')
 	
 	handlelink = urllib2.urlopen(anotherlink)
 	html_doclink = handlelink.read()
-	targetlink = open("test.html", 'w')
+	nameoffile = "data" + str(data) + ".html"
+	targetlink = open(nameoffile, 'w')
 	targetlink.write(html_doclink)
 	targetlink.close()
 	
@@ -27,5 +30,7 @@ for link in soup.findAll('a'):
 	savelink.write("\n")
 	print(anotherlink)
 	handlelink.close()
+	
+	data = data + 1
 
 savelink.close()

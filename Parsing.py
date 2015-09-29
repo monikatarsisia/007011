67c55eff7a7b76b350f48d9df0062dd8695631e8
Monika Tarsisia Nangoi <monikatarsisia@hotmail.com>
2015-09-29 07:27:54 -0700

    Update Parsing.py

--- Parsing.py
+++ Parsing.py
@@ -4,7 +4,7 @@ import urllib2
 thisurl = "http://bisniskeuangan.kompas.com/"
 handle = urllib2.urlopen(thisurl)
 html_doc =  handle.read()
-#handle.close()
+handle.close()
 
 target = open("dataOri.html", 'w')
 target.write(html_doc)
@@ -12,6 +12,20 @@ target.close()
 
 soup = BeautifulSoup(html_doc)
 
+savelink = open("hyperlink.html", 'w')
+
 for link in soup.findAll('a'):
-	anotherlink =link.get('href') 
+	anotherlink =link.get('href')
+	
+	handlelink = urllib2.urlopen(anotherlink)
+	html_doclink = handlelink.read()
+	targetlink = open("test.html", 'w')
+	targetlink.write(html_doclink)
+	targetlink.close()
+	
+	savelink.write(anotherlink)
+	savelink.write("\n")
 	print(anotherlink)
+	handlelink.close()
+
+savelink.close()

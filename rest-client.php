05d05a5d65d6d6dfdb05218c08fe86c606560076
Monika Tarsisia Nangoi <monikatarsisia@hotmail.com>
2015-11-04 21:16:34 -0800

    Create rest-client.php

---
+++ rest-client.php
@@ -0,0 +1,23 @@
+<?php
+
+if (isset($_GET["action"]) && isset($_GET["id"]) && isset($_GET["action"]) == "get_info") {
+	$info = file_get_contents('http://localhost/rest-api.php?action=get_info&id=' . $_GET["id"]);
+	echo $info;
+	echo '<br />';
+	echo '<br />';
+	$info = json_decode($info,true);
+#	echo $info;
+}
+
+?>
+
+<table>
+<tr> 
+	<td> ID </td>
+	<td> <?php echo $info["id"] ?> </td>
+</tr>
+<tr> 
+	<td> Name </td>
+	<td> <?php echo $info["name"] ?> </td>
+</tr>
+</table>
